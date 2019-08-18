82b11bbc524d7fe8bc9238b350bc058439c176d0
mfirulin <mfirulin@gmail.com>
2019-08-18 18:30:11 +0300

    First commit

---
+++ GetNameServlet/src/GetName.java
@@ -0,0 +1,30 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GetName
+ */
+@WebServlet("/GetName")
+public class GetName extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+	    response.setContentType("text/html");
+	    PrintWriter pw = response.getWriter();
+	    String name = request.getParameter("name");
+	    pw.println("Welcome " + name);
+	}
+
+}
